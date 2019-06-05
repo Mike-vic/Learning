2fe70ef285d41474af9b180d6f2ba854c750461a
Mike <mike@Compu-del-Maik.local>
2019-06-04 19:15:54 -0500

    Removing accents

---
+++ Train.js
@@ -0,0 +1,9 @@
+var Nom = "Mi hermano se llama 'Dave'.";
+Nom += "y tiene 23 años.\n\n";
+var NomCom = `Pero su nombre completo es: \n
+Jose
+\tDavid
+\t\tVictoria
+\t\t\tReyes`;
+
+console.log (`Hola. Soy Miguel. "${Nom} ${NomCom}`);
